6f0d3633b63d889683b9e3ebf69143c805dedda8
mirelinha12345 <141829141+mirelinha12345@users.noreply.github.com>
2024-11-08 08:20:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sociologia',
+    'De quem é a frase "Penso logo existo"?',
+    'Descartes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor país do mundo?',
+    'Vaticano'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas numeráis temm o número PI?',
+    'Infinitas'
+)
+
+criaCartao(
+    'química',
+    'Atualmente quantos elementos químicos posssui na tabela periódica?',
+    '118'
+)
